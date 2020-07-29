d0e5b53a2abcbef2bff427513225483a62d2a961
Maximilian Richt <maxi@richt.name>
2020-07-29 19:38:17 +0200

    pull bike rental station information for each tile

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const geojsonVt = require('geojson-vt');
 const vtPbf = require('vt-pbf');
 const request = require('requestretry');
 const zlib = require('zlib');
+const clone = require("clone");
 
 const query = `
   query bikerentals {
@@ -15,60 +16,66 @@ const query = `
     }
   }`
 
-class GeoJSONSource {
-  constructor(uri, callback){
-    uri.protocol = "http:"
-    request({
-      url: uri,
-      body: query,
-      maxAttempts: 120,
-      retryDelay: 30000,
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/graphql'
-      }
-    }, function (err, res, body){
-      if (err){
-        console.log(err)
-        callback(err);
-        return;
-      }
-
-      const geoJSON = {type: "FeatureCollection", features: JSON.parse(body).data.bikeRentalStations.map(station => ({
-        type: "Feature",
-        geometry: {type: "Point", coordinates: [station.lon, station.lat]},
-        properties: {
-          id: station.stationId,
-          name: station.name,
-          networks: station.networks.join()
-        }
-      }))}
-
-      this.tileIndex = geojsonVt(geoJSON, {
-        maxZoom: 20,
-        buffer: 256
-      }); //TODO: this should be configurable
-      console.log("city bikes loaded from:", uri.host + uri.path)
-      callback(null, this)
-    }.bind(this));
-  };
-
-  getTile(z, x, y, callback){
-    let tile = this.tileIndex.getTile(z, x, y)
-
-    if (tile === null){
-      tile = {features: []}
+const getTileIndex = (uri, callback) => {
+  request({
+    url: uri,
+    body: query,
+    maxAttempts: 120,
+    retryDelay: 30000,
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/graphql'
+    }
+  }, function (err, res, body){
+    if (err){
+      console.log(err)
+      callback(err);
+      return;
     }
 
-    const data = Buffer.from(vtPbf.fromGeojsonVt({stations: tile}));
+    const geoJSON = {type: "FeatureCollection", features: JSON.parse(body).data.bikeRentalStations.map(station => ({
+      type: "Feature",
+      geometry: {type: "Point", coordinates: [station.lon, station.lat]},
+      properties: {
+        id: station.stationId,
+        name: station.name,
+        networks: station.networks.join()
+      }
+    }))}
 
-    zlib.gzip(data, function (err, buffer) {
-      if (err){
-        callback(err);
-        return;
+    // console.log("city bikes loaded from:", this.uri.host + this.uri.path)
+    callback(null, geojsonVt(geoJSON, {
+      maxZoom: 20,
+      buffer: 256
+    }))  //TODO: this should be configurable
+  });
+}
+
+class GeoJSONSource {
+  constructor(uri, callback){
+    this.uri = clone(uri)
+    this.uri.protocol = "http:"
+    callback(null, this)
+  }
+
+  getTile(z, x, y, callback){
+   getTileIndex(this.uri, (err, tileIndex) => {
+      let tile = tileIndex.getTile(z, x, y)
+
+      if (tile === null) {
+        tile = {features: []}
       }
 
-      callback(null, buffer, {"content-encoding": "gzip"})
+      const data = Buffer.from(vtPbf.fromGeojsonVt({stations: tile}));
+
+      zlib.gzip(data, function (err, buffer) {
+        if (err){
+          callback(err);
+          return;
+        }
+
+        callback(null, buffer, {"content-encoding": "gzip"})
+      })
     })
   }
 
